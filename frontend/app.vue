761577a78adb412fdbb48abe50fe1e3fbbc1fee3
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2025-01-19 02:21:14 -0800

    add metadata

--- frontend/app.vue
+++ frontend/app.vue
@@ -6,3 +6,44 @@
 		</NuxtLayout>
 	</div>
 </template>
+
+<script setup lang="ts">
+	useHead({
+		title: "Weatheria",
+		meta: [
+			{
+				name: "description",
+				content:
+					"Predict Earth’s surface temperatures with advanced machine learning models. Analyze historical climate data and forecast spatial and temporal trends.",
+			},
+			{
+				name: "keywords",
+				content:
+					"earth temperature prediction, climate modeling, surface temperature, Weatheria, machine learning climate app",
+			},
+			{ name: "author", content: "Edwin Peraza" },
+
+			{
+				property: "og:title",
+				content: "Weatheria - Earth Surface Temperature Predictor",
+			},
+			{
+				property: "og:description",
+				content:
+					"Explore global climate trends and predict Earth’s surface temperatures using cutting-edge machine learning algorithms.",
+			},
+			{ property: "og:type", content: "website" },
+			{ property: "og:url", content: "https://www.weatheria.com" },
+			{ name: "twitter:card", content: "summary_large_image" },
+			{
+				name: "twitter:title",
+				content: "Weatheria - Earth Surface Temperature Predictor",
+			},
+			{
+				name: "twitter:description",
+				content:
+					"Discover insights into Earth’s surface temperature trends with Weatheria’s machine learning-powered predictions.",
+			},
+		],
+	});
+</script>
